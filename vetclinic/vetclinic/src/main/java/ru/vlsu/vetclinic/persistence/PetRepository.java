c8b2d5b234c86c921adea3ec6191ada1a55434af
Аnna Kotova <yuni.helm@mail.ru>
2021-06-04 16:31:06 +0300

    Организована страничка вывода списка животных

---
+++ vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/persistence/PetRepository.java
@@ -0,0 +1,10 @@
+package ru.vlsu.vetclinic.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//Репозиторий - класс, ответственный за сохранение сущностей в бд и подсасывание их оттудава
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Integer> {
+
+}
