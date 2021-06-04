072a50e500fac1de509ced8cf4bbcef642964aff
Аnna Kotova <yuni.helm@mail.ru>
2021-06-04 15:16:57 +0300

    Base skeleton for the application
    :)

---
+++ vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/VetclinicApplication.java
@@ -0,0 +1,13 @@
+package ru.vlsu.vetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetclinicApplication.class, args);
+	}
+
+}
