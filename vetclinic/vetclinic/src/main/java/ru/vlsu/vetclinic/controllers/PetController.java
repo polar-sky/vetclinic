2a0af360e0f0e85550f97ead4733de4c92148061
Juliya Ipatova <inu07040@mail.ru>
2021-06-06 17:01:22 +0300

    работает select

--- vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/controllers/PetController.java
+++ vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/controllers/PetController.java
@@ -4,25 +4,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.vlsu.vetclinic.persistence.Pet;
 import ru.vlsu.vetclinic.persistence.PetRepository;
+import ru.vlsu.vetclinic.persistence.PetType;
+import ru.vlsu.vetclinic.persistence.PetTypeRepository;
+
+import java.util.List;
 
 @Controller
 public class PetController {
 
-    private PetRepository rep;
+    private final PetRepository petRepo;
+    private final PetTypeRepository typeRepo;
 
     //autowired - при создании данного класса спринг будет искать есть ли класс, реализующий интерфейс petrepository и передаст этот класс в метод
     @Autowired
-    public PetController(PetRepository repository){
-        this.rep = repository;
+    public PetController(PetRepository petRepository, PetTypeRepository petTypeRepository){
+        this.petRepo = petRepository;
+        this.typeRepo= petTypeRepository;
     }
 
     //метод для возврата главной хтмл странички список животных
-    @GetMapping
-    public String indexPage(Model model){
+    @GetMapping("/")
+    public String petPage(Model model){
 
-        model.addAttribute("pets", rep.findAll());
+        List<Pet> pets;
+        pets = petRepo.findAll();
+        model.addAttribute("pets", pets);
+        return "pets";
+    }
 
-        return "index";
+    //метод для возврата странички видов животных *по приколу чтобы понять в чем сучья проблема
+    @GetMapping("/types")
+    public String typePage(Model model){
+
+        List<PetType> types;
+        types = typeRepo.findAll();
+        model.addAttribute("types", types);
+        return "types";
+    }
+
+    @GetMapping("/newpet")
+    public String createPet(Model model){
+        List<PetType> types = typeRepo.findAll();
+        Pet pet = new Pet();
+        model.addAttribute("pet", pet);
+        model.addAttribute("types", types);
+        return "newpet";
     }
 }
--- vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/security/SecurityConfig.java
+++ vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").permitAll()
                 .antMatchers("/authenticationUser").permitAll()
                 .antMatchers("/register").permitAll()
-                .antMatchers("/**").authenticated()
+                .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login").defaultSuccessUrl("/").failureUrl("/kekw")
