4f687c2f2f17148ba5b35022a26185e684f50791
Аnna Kotova <yuni.helm@mail.ru>
2021-06-15 04:01:35 +0300

    страничка вывода записей для клиента, возможность отменить прием

--- vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/controllers/EntryController.java
+++ vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/controllers/EntryController.java
@@ -1,14 +1,15 @@
 package ru.vlsu.vetclinic.controllers;
 
+import com.sun.xml.bind.v2.TODO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.vlsu.vetclinic.persistence.*;
-
+import java.util.Date;
 
 import java.security.Principal;
-import java.sql.Date;
+//import java.sql.Date;
 import java.util.List;
 
 @Controller
@@ -33,14 +34,22 @@ public class EntryController {
     //*ЗАЯВКИ*
     //метод для возврата странички списка заявок
     //изменила маппинг, чтобы записи выводились на страничку /entries, а не на index
-/*    @GetMapping("/entries")
-    public String entryPage(Model model, Principal principal){
-
+    @GetMapping("/entriesclient") ////////////////////////////для клиента
+    public String entryPageForClient(Model model, Principal principal) {
         List<Entry> entries;
         entries = entryRepo.findByClientidUsername(principal.getName());
         model.addAttribute("entries", entries);
-        return "entries";
-    }*/
+        return "entriesclient";
+    }
+
+    @GetMapping("/entriesvet") ////////////////////////////для врача
+    public String entryPageForVet(Model model, Principal principal) {
+        User user =userRepo.findByUsername(principal.getName()).get();
+        List<Entry> entries;
+        entries = entryRepo.findByVetid(user.getVetid().getId()); //здесь ошибка в представлении
+        model.addAttribute("entries", entries);
+        return "entriesvet";
+    }
 
     @GetMapping("/vets/newentry/{id}")
     public String createEntry(@PathVariable("id") Integer id, Model model, Principal principal){    //здесь типа передаётся айди врача, но скорее всего неправильно
@@ -49,7 +58,7 @@ public class EntryController {
         Vet vet = vetRepo.findById(id).get();
         List <Schedule> schedules = scheduleRepo.findByVetid(vet); //выпадающий список даты и времени
         Entry entry = new Entry();
-        //TODO сразу записываем в объект записи на прием врача так как выбрали его на прошлом этапе
+
         entry.setVetid(vet);
         model.addAttribute("entry", entry);
         model.addAttribute("schedules", schedules);
@@ -58,15 +67,29 @@ public class EntryController {
         return "newentry";
     }
 
-    @PostMapping("/save")
-    public String saveEntry(Entry entry, Principal principal){ //надо передать vetid
+    @PostMapping("/saveentry")
+    public String saveEntry(Entry entry, Principal principal){
         User user = userRepo.findByUsername(principal.getName()).get();
         entry.setClientid(user);
         entryRepo.save(entry);
-        //TODO удаляем время которое занял клиент см. findByDateAndVetid метод в репозитории schedule
         Schedule schedule = scheduleRepo.findByDateAndVetid(entry.getDate(), entry.getVetid()) ;
         scheduleRepo.delete(schedule);
-        return "redirect:/";
+        return "redirect:/entriesclient";
+
     }
 
+    @GetMapping("/entriesclient/delete/{id}") //удалять сможет только клиент
+    public String showDeleteEntry(@PathVariable("id") Integer id){
+        Entry entry = entryRepo.findById(id).get();
+        Date date = java.sql.Date.valueOf(java.time.LocalDate.now());
+        if (entry.getDate().compareTo(date)==1)      //надо создавать свободную дату только если она свежая
+        {
+            Schedule schedule = new Schedule();   //теперь восстанавливаем свободную запись, место к врачу освободилось
+            schedule.setVetid(entry.getVetid());
+            schedule.setDate(entry.getDate());
+            scheduleRepo.save(schedule);
+        }
+        entryRepo.deleteById(id);
+        return "redirect:/entriesclient";
+    }
 }
