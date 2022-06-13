f5958e4d26dac5ddeb7efe2c32729d9772a5e8a7
Inu07040 <inu07040@mail.ru>
2022-06-13 22:12:44 +0300

    готово?

--- vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/VetclinicApplication.java
+++ vetclinic/vetclinic/src/main/java/ru/vlsu/vetclinic/VetclinicApplication.java
@@ -1,13 +1,11 @@
 package ru.vlsu.vetclinic;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@Slf4j
 public class VetclinicApplication {
 
 	@Bean
@@ -17,9 +15,6 @@ public class VetclinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VetclinicApplication.class, args);
-			log.info("saleh sasat");
-			log.warn("saleh sasat");
-			log.error("saleh sasat");
 
 	}
 
